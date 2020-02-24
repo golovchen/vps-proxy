b8faae94b15c329917474ea84ab0f599fd762326
Dmitry Golovchenko <golovchen@gmail.com>
2020-02-24 19:51:09 +0300

    Rm :latest, add pull

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 docker network create proxy-net
 
-docker container run --name mindmap --network proxy-net --detach golovchen/mindmap:latest
-docker container run --name golovchen --network proxy-net --detach golovchen/golovchen.com:latest
+docker pull golovchen/mindmap
+docker container run --name mindmap --network proxy-net --detach golovchen/mindmap
+docker container run --name golovchen --network proxy-net --detach golovchen/golovchen.com
 docker container run --name vps-proxy --network proxy-net --detach --publish 80:80 golovchen/vps-proxy
