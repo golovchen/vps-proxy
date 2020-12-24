fa0e6e53469483e9df186115495a3a3a9ef1c943
Dmitry Golovchenko <golovchen@gmail.com>
2020-12-24 12:33:07 +0300

    run.sh cleanup

--- run.sh
+++ run.sh
@@ -5,5 +5,4 @@ docker network create proxy-net
 docker pull golovchen/mindmap
 docker container run --name mindmap --network proxy-net --detach golovchen/mindmap
 docker container run --name golovchen --network proxy-net --detach golovchen/golovchen.com
-docker container run --name geork --network proxy-net --detach golovchen/geork
 docker container run --name vps-proxy --network proxy-net --detach --publish 80:80 golovchen/vps-proxy
